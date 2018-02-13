7d2cd048c753d4a7d8eb2689b068ccb4068b84ac
Igor Antonov <antonov.igor.khv@gmail.com>
2018-02-13 15:24:20 +1000

    Добавляет google map

---
+++ source/js/google-map.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function initMap() {
+  var map = document.querySelector('#map');
+  var chords = { lat: 59.938756, lng: 30.3230691 };
+  var image = {
+    url: '../img/icons/icon-map-marker.svg',
+    size: new google.maps.Size(36, 36),
+    anchor: new google.maps.Point(15, 15)
+  };
+
+  var map = new google.maps.Map(map, { zoom: 16, center: chords });
+  var marker = new google.maps.Marker( { position: chords, map: map, icon: image, title: 'Pink' });
+}
+
+
+
+
+
+
